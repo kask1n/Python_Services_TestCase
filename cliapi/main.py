7dac5ded8b9b43b48aaae2f18e14b002c43717b7
kask1n <119409700+kask1n@users.noreply.github.com>
2023-12-16 23:27:57 +0300

    develop: Added folder structure

--- cliapi/main.py
+++ cliapi/main.py
@@ -5,7 +5,6 @@ import time
 from string import ascii_letters
 from random import randint, choice, choices
 
-import docker
 import requests
 from fastapi import HTTPException
 
@@ -35,35 +34,20 @@ def make_post_request(url_src, data_src):
         raise HTTPException(status_code=418, detail="Что-то пошло не так")
 
 
-THREADS = os.environ["THREADS"]
-DELAY = os.environ["DELAY"]
+if __name__ == '__main__':
+    THREADS = os.environ["THREADS"]
+    DELAY = os.environ["DELAY"]
 
-# client = docker.from_env()
-# container = client.containers.get("mywebapi")
-# container_info = container.attrs
-#
-# if 'NetworkSettings' in container_info and 'IPAddress' in container_info:
-#     ip_addr = container_info['NetworkSettings']['IPAddress']
-# else:
-#     ip_addr = None
-#
-# print(ip_addr)
-#
-# if ip_addr:
-#     url = f'http://{ip_addr}:8000/api/data'
-# else:
-#     url = f'http://localhost:8000/api/data'
+    url = 'http://177.77.0.78:8000/api/data'
 
-url = 'http://177.77.0.3:8000/api/data'
-# url = f'http://localhost:8000/api/data'
+    for i in range(int(THREADS)):
+        time.sleep(randint(0, int(DELAY)) / 1000)
 
-for i in range(int(THREADS)):
-    time.sleep(randint(0, int(DELAY)) / 1000)
+        gen_data = generate_text()
+        data = {"log": f"{gen_data}"}
 
-    gen_data = generate_text()
-    data = {"log": f"{gen_data}"}
+        logging.basicConfig(filename='client.log', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info(f"URL: {url} - Text: '{gen_data}'")
 
-    logging.basicConfig(filename='client.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    logging.info(f"URL: {url} - Text: '{gen_data}'")
-
-    threading.Thread(target=make_post_request(url, data)).start()
+        threading.Thread(target=make_post_request(url, data)).start()
